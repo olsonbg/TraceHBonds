c05e6c8727a9ed3b4ed8e23dda4a14d5ad4798ab
olsonbg <olsonbg@gmail.com>
2015-10-27 19:42:05 -0500

    Scripts for generating blender images
    Calling hbchain.py from blender on the command line, with the output of
    TraceHBonds with both the --sizehist and --json flags, will generate a
    nice image. An animation will also be setup.

---
+++ blender/hbchain.py
@@ -0,0 +1,424 @@
+#!/usr/bin/python
+
+#------------------------------------------------------------------------------
+# Name:        hbhcain.py
+# Purpose:     Show hydrogen bonded chains in blender using the output from
+#              TraceHBonds --json. An animation is also setup, showing a
+#              rotating PBC box. Uses tubewithcaps.py for generating tubes, and
+#              diverging_map.py for coloring tubes by length with a divergent
+#              color map.
+#
+# Author:      Brian G. Olson (olsonbg@gmail.com)
+#
+# Created:     27 October 2015
+# Copyright:   (c) 2015
+#
+# Usage:       blender -P hbchain.py -- -f HBonds1.json
+#
+# For help:    blender -P hbchain.py -- --help
+#------------------------------------------------------------------------------
+#
+
+import array
+import numpy as np
+import json
+import bpy,bmesh
+import mathutils
+from random import random
+# Import from the same directory this script is run from
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+
+from tubewithcaps import TubeWithCaps
+from diverging_map import ColorMapCreator
+
+import time
+start_time = time.time()
+
+NumFrames = 120
+Scale =  5.0
+Radius = 0.1
+WhiteIndex = 3
+
+#
+# used for testing
+#
+def put_marker( co, name="Test" ):
+	obj_empty = bpy.data.objects.new(name, None)
+	bpy.context.scene.objects.link(obj_empty)
+	obj_empty.location = co
+
+# Add PBC box, camera, and light source
+def draw_box(box, scale):
+	PBC = box["PBC"][0]["xyz"]
+	PBC_2 = [ x/2.0/scale for x in PBC ]
+
+	# Empty object used for rotation/animation, placed at center of PBC box,
+	# and will also be the parent of the camera and lamps.
+	bpy.ops.object.empty_add(type='CUBE',radius=0.1, location=(PBC_2))
+	bpy.context.object.name = 'RotationBox'
+	empty = bpy.data.objects['RotationBox']
+
+	bpy.ops.mesh.primitive_cube_add(radius=PBC_2[0],location=(PBC_2))
+	bpy.ops.object.modifier_add(type='WIREFRAME')
+	bpy.context.object.modifiers["Wireframe"].use_relative_offset = True
+	bpy.context.object.modifiers["Wireframe"].use_even_offset = False
+	bpy.context.object.name = 'PBC'
+
+	# Add camera
+	# Looking down the z axis, and rotating around the y axis on animation.
+	bpy.ops.object.camera_add(view_align=True, location=(0,0,10*PBC_2[2]) )
+	bpy.ops.object.constraint_add(type='TRACK_TO')
+	bpy.context.object.constraints["Track To"].up_axis = 'UP_Y'
+	bpy.context.object.name = 'Camera'
+	bpy.data.objects['Camera'].parent = empty
+
+	# Add light source
+
+	# Create new lamp datablock
+	lamp_data = bpy.data.lamps.new(name="lamp-center", type='SUN')
+
+	# Create new object with our lamp datablock
+	lamp_object = bpy.data.objects.new(name="lamp-center",
+	                                   object_data=lamp_data)
+	lamp_data.use_nodes = True
+
+	#  Link lamp object to the scene so it'll appear in this scene
+	bpy.context.scene.objects.link(lamp_object)
+
+	lamp_object.location = ( 0, 0, 10.1 * PBC_2[2] )
+
+	bpy.data.objects['lamp-center'].parent = empty
+
+	nodes = bpy.data.lamps['lamp-center'].node_tree.nodes
+	# clear all nodes to start clean
+	for node in nodes:
+		nodes.remove(node)
+
+	# create emission node
+	node_emission = nodes.new(type='ShaderNodeEmission')
+	node_emission.inputs[0].default_value=(1,1,1,1) # While RGBA
+	node_emission.inputs[1].default_value = 3.0 # Strength
+	node_emission.location = 0,0
+
+	# create output node
+	node_output = nodes.new(type='ShaderNodeOutputMaterial')
+	node_output.location = 200,0
+
+	# Link nodes
+	links = bpy.data.lamps['lamp-center'].node_tree.links
+	link = links.new(node_emission.outputs[0], node_output.inputs[0])
+
+
+
+def draw_molecule(molecule, i, radius, scale):
+	coordinates = list()
+	numatoms = len(molecule["atoms"])
+
+	for atom in molecule["atoms"]:
+		coordinates.append( [ x/scale for x in atom["location"]])
+
+	num_co = len(coordinates)
+	tubeName   = 'Chain{0:04d}'.format(i)
+	groupName  = 'GroupLength{0:02d}'.format(num_co)
+	lengthName = 'Length{0:02d}'.format(num_co)
+
+	# If groupName doesn't exist, then neither does the empty LengthName
+	if not groupName in bpy.data.groups:
+		bpy.ops.group.create(name=groupName)
+		# Make empty as parent for chains of this length
+		bpy.ops.object.empty_add(type='CUBE',radius=0.1, location=(0,0,0))
+		bpy.context.object.name = lengthName
+		bpy.context.object.parent = bpy.data.objects['Chains']
+
+	group     = bpy.data.groups[groupName]
+	ob_length = bpy.data.objects[lengthName]
+
+	tube = TubeWithCaps(tubeName, radius, coordinates)
+	ob_tube = tube.make()
+
+	ob_tube.parent = ob_length
+	group.objects.link(ob_tube)
+
+	bpy.ops.object.select_all(action='DESELECT')
+	return numatoms
+
+def material_nodes(mat, color):
+	mat.use_nodes = True
+	nodes = mat.node_tree.nodes
+
+	# clear all nodes to start clean
+	for node in nodes:
+		nodes.remove(node)
+
+	node_rgba = nodes.new(type='ShaderNodeRGB')
+	node_rgba.outputs[0].default_value = color
+	node_rgba.location = (-200,400)
+
+	node_rgbaBack = nodes.new(type='ShaderNodeRGB')
+	node_rgbaBack.outputs[0].default_value=( 0, 0, 0, 1)
+	node_rgbaBack.location = (-200,200)
+
+	node_mix = nodes.new(type='ShaderNodeMixRGB')
+	node_mix.inputs[0].default_value = 0.7
+	node_mix.location = (0, 200)
+
+	node_layerweight = nodes.new(type='ShaderNodeLayerWeight')
+	node_layerweight.inputs[0].default_value = 0.5
+	node_layerweight.location = (200,500)
+
+	node_bsdf1 = nodes.new(type='ShaderNodeBsdfDiffuse')
+	node_bsdf1.inputs[1].default_value = 0.0
+	node_bsdf1.location = (200,350)
+
+	node_bsdf2 = nodes.new(type='ShaderNodeBsdfDiffuse')
+	node_bsdf2.inputs[1].default_value = 0.0
+	node_bsdf2.location = (200,200)
+
+	node_fresnel = nodes.new(type='ShaderNodeFresnel')
+	node_fresnel.inputs[0].default_value = 1.450
+	node_fresnel.location = (400, 500)
+
+	node_mixshader1 = nodes.new(type='ShaderNodeMixShader')
+	node_mixshader1.location = (400, 350)
+
+	node_glossy = nodes.new(type='ShaderNodeBsdfGlossy')
+	node_glossy.inputs[0].default_value = (1,1,1,1)
+	node_glossy.inputs[1].default_value = 0.050
+	node_glossy.location = (400, 200)
+
+	node_mixshader2 = nodes.new(type='ShaderNodeMixShader')
+	node_mixshader2.location = (600, 350)
+
+	node_output = nodes.new(type='ShaderNodeOutputMaterial')
+	node_output.location = (800, 350)
+
+	links = mat.node_tree.links
+	links.new(node_rgba.outputs[0], node_mix.inputs[1])
+	links.new(node_rgba.outputs[0], node_bsdf1.inputs[0])
+	links.new(node_rgbaBack.outputs[0], node_mix.inputs[2])
+	links.new(node_mix.outputs[0], node_bsdf2.inputs[0])
+	links.new(node_layerweight.outputs[1], node_mixshader1.inputs[0])
+	links.new(node_bsdf1.outputs[0], node_mixshader1.inputs[1])
+	links.new(node_bsdf2.outputs[0], node_mixshader1.inputs[2])
+	links.new(node_fresnel.outputs[0], node_mixshader2.inputs[0])
+	links.new(node_mixshader1.outputs[0], node_mixshader2.inputs[1])
+	links.new(node_glossy.outputs[0], node_mixshader2.inputs[2])
+	links.new(node_mixshader2.outputs[0], node_output.inputs[0])
+
+
+def generate_colormap(num_low, num_high):
+	# ColorMap for chain lengths
+	RGB1  = np.array([ 59,  76, 192])
+	RGB2  = np.array([180,   4,  36])
+	WHITE = np.array([221, 221, 221])
+
+	colormap1 = ColorMapCreator(RGB1,  WHITE, numColors=num_low,    quiet=True)
+	colormap2 = ColorMapCreator(WHITE, RGB2,  numColors=num_high+1, quiet=True)
+
+	ColorMap = colormap1.colorMap.tolist()[:] + colormap2.colorMap.tolist()[1:]
+
+	for i,c in enumerate(ColorMap):
+		c.append(1)
+		#  print("color[{:d}]: {:f} {:f}  {:f}  {:f} ".format(i,c[0], c[1], c[2], c[3]))
+
+	return ColorMap
+
+# Make materials for objects:
+#   1 per group
+#   1 for PBC box
+#
+def generate_materials(white_index):
+	import re
+
+	# The number of materials to generatre depends on the maximum chainlength
+	mat_count = 0
+	for group in bpy.data.groups:
+		if group.name.startswith("GroupLength"):
+			regex = re.compile(r'\d+')
+			j = int( regex.search(group.name).group(0) )
+			i = int( (j - 1)/2 )
+			if i > mat_count:
+				mat_count = i
+
+	if mat_count < white_index:
+		colormap = generate_colormap(mat_count, 0)
+	else:
+		colormap = generate_colormap(white_index, mat_count-white_index)
+
+	# Generate textures
+	for i in range(mat_count):
+		mat_name = "GroupLength{0:02d}".format(2*i+3)
+		mat = bpy.data.materials.new(mat_name)
+		material_nodes(mat, colormap[i])
+
+
+	# Also add a material for the PBC box
+	mat = bpy.data.materials.new("PBCMaterial")
+	mat.diffuse_color = 0.0, 0.0, 0.0
+	# Don't want the PBC box to shine much at all when directly in the light
+	mat.specular_intensity = 0.02
+
+
+#
+# Assign generated materials to objects
+#
+def assignMaterials():
+	# Assign a texture to each chain length group
+
+	for group in bpy.data.groups:
+		mat = bpy.data.materials[group.name]
+		#  print("Group: {0:s}".format(group.name))
+		for object in group.objects:
+			#  print("  Chain: {0:s}".format(object.name))
+			if object.data is not None:
+				if len(object.material_slots) < 1:
+					object.data.materials.append(mat)
+				else:
+					object.material_slots[0].material = mat
+
+	# Give the PBC box its material
+	ob = bpy.data.objects["PBC"]
+	mat = bpy.data.materials["PBCMaterial"]
+	if len(ob.material_slots) < 1:
+		ob.data.materials.append(mat)
+	else:
+		ob.material_slots[0].material = mat
+	ob.modifiers["Wireframe"].material_offset = 0
+
+
+
+#
+# Animation settings
+#
+def setupAnimation( numframes ):
+	#bpy.ops.object.select_all(action='DESELECT')
+	rotbox = bpy.data.objects['RotationBox']
+
+	bpy.context.scene.frame_start =  1
+	bpy.context.scene.frame_end   =  numframes
+
+	# Initial rotation
+	rotbox.rotation_euler[0] = 0
+	rotbox.rotation_euler[1] = 0
+	rotbox.rotation_euler[2] = 0
+	# Set this rotation as initial keyframe
+	rotbox.keyframe_insert("rotation_euler", frame=1)
+
+	# Final rotation
+	rotbox.rotation_euler[0] = 0
+	rotbox.rotation_euler[1] = 6.28319
+	rotbox.rotation_euler[2] = 0
+	# Set this rotation as keyframe
+	rotbox.keyframe_insert("rotation_euler", frame=numframes + 1)
+
+	# Use linear interpolation along frames
+	for fcur in rotbox.animation_data.action.fcurves:
+		fcur.extrapolation='LINEAR'
+
+
+def main():
+	import sys       # to get command line args
+	import argparse  # to parse options for us and print a nice help message
+
+	# get the args passed to blender after "--", all of which are ignored by
+	# blender so scripts may receive their own arguments
+	argv = sys.argv
+
+	if "--" not in argv:
+		argv = []  # as if no args are passed
+	else:
+		argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+    # When --help or no args are given, print this help
+	usage_text = \
+	"Run blender with this script:\n\n" \
+	"  blender --python " + __file__ + " -- [options]"
+
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description=usage_text)
+
+	# Possible types are: string, int, long, choice, float and complex.
+	parser.add_argument("-f", "--file", dest="filename", type=str,
+	                    metavar='FILE', required=True,
+	                    help="This file will be loaded")
+
+	parser.add_argument("-r", "--radius", dest="radius", type=float,
+	                    required=False, default=Radius,
+	                    help="Radius of the tubes. (default: %(default)d)")
+
+	parser.add_argument("-n", "--numframes", dest="frames", type=int,
+	                    required=False, default=NumFrames,
+	                    help="Number of frames rendered for animation. (default: %(default)d)")
+
+	parser.add_argument("-s", "--scale", dest="scale", type=float,
+	                    required=False, default=Scale,
+	                    help="Scale coordinates down by this factor. (default: %(default)f)")
+
+	parser.add_argument("-w", "--white", dest="white_index", type=int,
+	                    required=False, default=WhiteIndex,
+	                    help="Location of 'white' in the divergent colormap. From the shortest chain to WHITE_INDEX the color will go blue to white. From WHITE_INDEX to the longest chain the color will go white to red. (default: %(default)d)")
+
+	args = parser.parse_args(argv)
+
+	if not argv:
+		parser.print_help()
+		return
+
+	# Load data
+	json_data=open(args.filename).read()
+	molecules = json.loads(json_data)
+
+	scene = bpy.context.scene
+
+	# Use Cycles render engine
+	scene.render.engine = 'CYCLES'
+	scene.cycles.samples = 200
+	scene.cycles.preview_samples = 20
+	scene.cycles.blur_glossy = 0.50
+	scene.world.horizon_color = ( 101/255.0, 123/255.0, 131/255.0 )
+
+	# Start with an empty scene
+	bpy.ops.object.select_all(action='DESELECT')
+	bpy.ops.object.select_all(action='TOGGLE')
+	bpy.ops.object.delete(use_global=False)
+
+	# Remove any materials which may be left
+	for material in bpy.data.materials:
+	    material.user_clear();
+	    bpy.data.materials.remove(material);
+
+	# Empty object used as parent for chains.
+	bpy.ops.object.empty_add(type='CUBE',radius=0.1, location=(0,0,0))
+	bpy.context.object.name = 'Chains'
+
+	i = 0
+	for mol in molecules:
+		if "PBC" in mol:
+			# Add PBC box, camera, and light source
+			draw_box(mol,args.scale)
+		if "atoms" in mol:
+			#  print("Drawing chain {0:d}.".format(i))
+			draw_molecule(mol, i, args.radius, args.scale)
+			i = i+1
+
+
+	print("Number of chains: {0:d}.".format(i))
+
+	generate_materials(args.white_index)
+	assignMaterials()
+
+	setupAnimation(args.frames)
+
+	# Turn off relationship lines in all views
+	for area in bpy.context.screen.areas:
+		if area.type == 'VIEW_3D':
+			area.spaces[0].show_relationship_lines = False
+
+	bpy.data.objects["RotationBox"].hide        = True
+	bpy.data.objects["RotationBox"].hide_render = True
+
+
+
+if __name__ == "__main__":
+	main()
+	print("--- %s seconds ---" % (time.time() - start_time))
---
+++ blender/tubewithcaps.py
@@ -0,0 +1,253 @@
+#!/usr/bin/python
+#------------------------------------------------------------------------------
+# Name:        tubewithcaps.py
+# Purpose:     Generate tubes in blender which follow a path. The tubes have a
+#              circular profile, and hemispherical end caps.
+#
+# Author:      Brian G. Olson (olsonbg@gmail.com)
+#
+# Created:     27 October 2015
+# Copyright:   (c) 2015
+#------------------------------------------------------------------------------
+
+import array
+import numpy as np
+import bpy,bmesh
+import mathutils
+from random import random
+
+class TubeWithCaps(object):
+
+	def __init__(self, name, radius, coordinates):
+		self.name        = name
+		self.radius      = radius
+		self.coordinates = coordinates
+		self.startCapName= "CapStart"
+		self.endCapName  = "CapEnd"
+		self.pathName    = "Path"
+		self.bevelName   = "ChainBevel"
+
+
+	#
+	# Bevel object for curves
+	#
+	def make_bevelobject(self):
+		# Circle for bevel object of curves (tubes).
+		bpy.ops.curve.primitive_bezier_circle_add( radius=self.radius,
+		                                           location=(0,0,0) )
+		bpy.context.object.name = self.bevelName
+
+		return bpy.data.objects[self.bevelName]
+
+
+	# Hemisphere on positive z.
+	def makeHemisphere(self, objname, segments):
+
+		ob = bpy.ops.mesh.primitive_uv_sphere_add(segments=segments,
+		                                          location=(0,0,0),
+		                                          size=self.radius)
+		bpy.ops.object.shade_smooth()
+		bpy.context.object.name = objname
+
+		bpy.ops.object.mode_set(mode='EDIT')
+		mesh=bmesh.from_edit_mesh(bpy.context.object.data)
+		for v in mesh.verts:
+			if v.co[2] <  0.0:
+				v.select = True
+			else:
+				v.select = False
+				#  mesh.verts.remove(v)
+
+		bpy.ops.mesh.delete(type='VERT')
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+		return bpy.data.objects[objname]
+
+
+	def make_caps(self, num_hemisphere_verts):
+		segments = num_hemisphere_verts
+		startCap = self.makeHemisphere(self.startCapName, segments)
+
+		# Make a copy for the endCap
+		endCap          = startCap.copy()
+		endCap.location = startCap.location
+		endCap.data     = startCap.data.copy() # duplicate mesh
+		endCap.name     = self.endCapName
+
+		bpy.context.scene.objects.link(endCap)
+
+		return startCap, endCap
+
+
+	def merge_verts(self, tube):
+		bm = bmesh.new()
+
+		bpy.ops.object.mode_set(mode='EDIT')
+		bm = bmesh.from_edit_mesh(tube.data) # Fill bmesh with object mesh
+
+		# get vertices of merged tube and caps which have 3 connections. They
+		# should be on the ends of the tube, and the caps
+		tube_end_verts = self.verts_withNconnections(bm, 3)
+
+		num_verts = int(len(tube_end_verts)/4)
+
+		v1 = tube_end_verts[:num_verts]
+		v2 = tube_end_verts[3*num_verts:4*num_verts]
+		v3 = tube_end_verts[num_verts:2*num_verts]
+		v4 = tube_end_verts[2*num_verts:3*num_verts]
+
+		# v1, v2, v3, and v4 should be same length
+		for i in range(len(v1)):
+			# v1 and v2 run in opposite directions, so use j for v2 and i for
+			# v1.
+			j = len(v1)-i-1+1
+			if i == 0:
+				j = 0
+
+			# Put the vertices which should be merged at the same location so
+			# that remove_doubles will have no problem finding them
+			v2[j].co = v1[i].co
+			v4[i].co = v3[i].co
+
+		bmesh.ops.remove_doubles(bm, verts=v1+v2)
+		bmesh.ops.remove_doubles(bm, verts=v3+v4)
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+
+
+	# Get all vertices which have N edges.
+	def verts_withNconnections(self, bm, N):
+
+		verts = [ v for v in bm.verts if len(v.link_edges) == N]
+
+		return verts
+
+
+	def verts_withNconnections_co(self, ob, N):
+		# Use bmesh
+		bm = bmesh.new()
+		bm.from_mesh(ob.data) # Fill bmesh with object mesh
+
+		# Get all vertices which have N edges.
+		verts = self.verts_withNconnections(bm, N)
+
+		# Get the coordinates of the the vertices
+		verts_co = [ (ob.matrix_world * v.co) for v in verts]
+
+		return verts_co
+
+	def extrude_tube(self):
+
+		polyline, curvedata = self.make_polyline()
+
+		bevel_ob = self.make_bevelobject()
+		curvedata.bevel_object = bevel_ob
+
+		# Convert to a mesh
+		bpy.data.objects[self.name].select = True
+		bpy.ops.object.convert(target='MESH')
+
+		# Done with bevel object, so delete it.
+		bpy.ops.object.select_all(action='DESELECT')
+		bevel_ob.select = True
+		bpy.ops.object.delete()
+
+		return polyline
+		#  return bpy.data.objects[self.name]
+
+
+	def make_polyline(self):
+
+		curvedata = bpy.data.curves.new(name=self.pathName, type='CURVE')
+		curvedata.dimensions = '3D'
+
+		objectdata = bpy.data.objects.new(self.name, curvedata)
+		objectdata.location = (0,0,0) #object origin
+		bpy.context.scene.objects.link(objectdata)
+
+		polyline = curvedata.splines.new('NURBS')
+		polyline.points.add(len(self.coordinates)-1)
+
+		w  = 100.0 # Weight of control points
+		for i, coords in enumerate(self.coordinates):
+			x,y,z = coords
+			polyline.points[i].co = (x,y,z,w)
+
+		polyline.order_u = len(polyline.points)-1
+
+		# Make curve touch endpoints
+		polyline.use_endpoint_u = True
+
+		return objectdata, curvedata
+
+
+	def aligncap(self, tube_end_co, cap, cap_normal, location):
+
+		# Get normal to tube end. Pick three points equally spaced to determine
+		# normals
+		one_third = int( len(tube_end_co)/3 )
+		end = [ tube_end_co[0],
+		        tube_end_co[one_third],
+		        tube_end_co[2*one_third] ]
+
+		normal = mathutils.geometry.normal(*end)
+		normal_sphere = mathutils.Vector( (0,0,-1) )
+		to_rotate = cap_normal.rotation_difference(normal)
+		cap.rotation_mode = 'QUATERNION'
+		# Rotate the cap so that it is at the same angle as the tube ends
+		cap.rotation_quaternion = to_rotate
+
+		# Move cap to the tube end
+		cap.location = location
+
+		# Apply transforms for later calculations.
+		bpy.ops.object.transform_apply(scale=True)
+
+		# Rotate cap to align vertices with tube vertices. The vertices will
+		# be very close to aligned, but not perfectly.
+		capverts = self.verts_withNconnections_co(cap, 3)
+		v1 = capverts[0]    - mathutils.Vector( location )
+		v2 = tube_end_co[0] - mathutils.Vector( location )
+		anglediff = v1.rotation_difference(v2)
+		cap.delta_rotation_quaternion = -1.0*anglediff
+
+
+	def make(self):
+		#  bpy.data.objects["ChainBevel"].select = False
+		tube = self.extrude_tube()
+		#  tube = bpy.data.objects[self.name]
+
+		# Get vertices of tube which have 3 connections. They should be on the
+		# ends of the tube.
+		tube_end_verts_co = self.verts_withNconnections_co(tube, 3)
+		num_verts = int(len(tube_end_verts_co)/2)
+
+		# Split verts in half. The vStart, and second half vEnd
+		tube_start_co = tube_end_verts_co[:num_verts]
+		tube_end_co   = tube_end_verts_co[num_verts:]
+
+		# Make hemispheres with same number of segments as the tube
+		capStart, capEnd = self.make_caps( num_verts )
+
+		self.aligncap( tube_start_co,
+		               capStart,
+		               mathutils.Vector( (0,0,-1) ),
+		               self.coordinates[0]  )
+
+		self.aligncap( tube_end_co,
+		               capEnd,
+		               mathutils.Vector( (0,0,1) ),
+		               self.coordinates[-1] )
+
+		# Join caps and tube
+		# The active object will be the name of the joined objects.
+		capEnd.select   = True
+		tube.select     = True
+		capStart.select = True
+		bpy.context.scene.objects.active = tube
+
+		bpy.ops.object.join()
+
+		self.merge_verts(tube)
+
+		return tube
